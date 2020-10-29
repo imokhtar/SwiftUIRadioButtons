518b7737bb680b25e4ffe59fdf60b1e3c1a566f8
Ahmed Mokhtar <>
2020-10-29 17:25:12 +0200

    v0.0.1

--- Sources/SwiftUIRadioButtons/RadioCollection.swift
+++ Sources/SwiftUIRadioButtons/RadioCollection.swift
@@ -49,6 +49,7 @@ internal struct RadioCollection<Data, Content>: View where Data: RandomAccessCol
 
 @available(iOS 13.0, *)
 public extension View {
+    /// sets the foreground color of the radio image in any `RadioCollection` view
     func radioForegroundColor(_ color: Color) -> some View {
         self.modifier(RadioForegroundColor(color: color))
     }
